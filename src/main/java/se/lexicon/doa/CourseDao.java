1eb04e3bf19280409260166511658e8baef244f5
Mattias Hellman <mattias.hellman@gmail.com>
2025-11-13 13:36:27 +0100

    Add interface CourseDao

--- src/main/java/se/lexicon/doa/CourseDao.java
+++ src/main/java/se/lexicon/doa/CourseDao.java
@@ -1,4 +1,15 @@
 package se.lexicon.doa;
 
+import se.lexicon.model.Course;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface CourseDao {
+    Course save(Course course);
+    Course findById(int id);
+    List<Course> findByName(String name);
+    List<Course> findByDate(LocalDate date);
+    List<Course> findAll();
+    boolean delete(Course student);
 }
