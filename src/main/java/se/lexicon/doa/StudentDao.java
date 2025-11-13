c41695797a1c31e691163ad76b0e5b1a81798101
Mattias Hellman <mattias.hellman@gmail.com>
2025-11-13 13:36:35 +0100

    Add interface StudentDao

--- src/main/java/se/lexicon/doa/StudentDao.java
+++ src/main/java/se/lexicon/doa/StudentDao.java
@@ -1,4 +1,16 @@
 package se.lexicon.doa;
 
+import se.lexicon.model.Course;
+import se.lexicon.model.Student;
+
+import java.util.List;
+
 public interface StudentDao {
+    Student save(Student student);
+    Student findByEmail(String email);
+    List<Student> findByName(String name);
+    Student findById(int id);
+    List<Student> findAll();
+    boolean delete(Student student);
+
 }
