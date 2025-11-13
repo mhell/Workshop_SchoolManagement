78b18fc80c109632da8c54625f3d2505cdc6af24
Mattias Hellman <mattias.hellman@gmail.com>
2025-11-13 10:18:39 +0100

    Add Course

---
+++ src/main/java/se/lexicon/model/Course.java
@@ -0,0 +1,12 @@
+package se.lexicon.model;
+
+import java.time.LocalDate;
+
+public class Course {
+    private int id;
+    private String courseName;
+    private LocalDate startDate;
+    private int weekDuration;
+    private List<Student> students;
+
+}
