68126b514e9b2afead8cbdb903a665cb4eee0bd6
Mattias Hellman <mattias.hellman@gmail.com>
2025-11-14 17:21:46 +0100

    Update CLI

--- src/main/java/se/lexicon/ui/CommandLineInterface.java
+++ src/main/java/se/lexicon/ui/CommandLineInterface.java
@@ -2,14 +2,16 @@ package se.lexicon.ui;
 
 import se.lexicon.dao.CourseDao;
 import se.lexicon.dao.StudentDao;
+import se.lexicon.model.Course;
 import se.lexicon.model.Student;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class CommandLineInterface {
     private final Scanner scanner = new Scanner(System.in);
-    private StudentDao studentDao;
-    private CourseDao courseDao;
+    private final StudentDao studentDao;
+    private final CourseDao courseDao;
 
     public CommandLineInterface(StudentDao studentDao, CourseDao courseDao) {
         this.studentDao = studentDao;
@@ -29,7 +31,7 @@ public class CommandLineInterface {
                 "0. Quit \n" +
                 "> ");
 
-        switch (scanner.next()) {
+        switch (scanner.nextLine()) {
             case "1":
                 create();
                 break;
@@ -44,10 +46,11 @@ public class CommandLineInterface {
                 break;
             case "0":
                 System.exit(0);
-            default:
-                mainMenu();
         }
+        mainMenu();
     }
+
+    //region <Create>
 
     private void create() {
         System.out.print("Create course or student: \n" +
@@ -56,24 +59,47 @@ public class CommandLineInterface {
                 "0. Main menu \n" +
                 "> ");
 
-        switch (scanner.next()) {
+        switch (scanner.nextLine()) {
             case "1":
                 newCourse();
+                break;
             case "2":
                 newStudent();
+                break;
             case "0":
                 mainMenu();
-            default:
-                create();
+                return;
         }
         create();
     }
 
     private void newCourse() {
+        System.out.print("Enter course name: ");
+        String courseName = scanner.nextLine();
+        System.out.print("Enter start date (e.g. 2007-12-03): ");
+        LocalDate startDate;
+        try {
+            startDate = LocalDate.parse(scanner.nextLine());
+        } catch (RuntimeException e) {
+            System.out.println("Error: " + e.getMessage());
+            newCourse();
+            return;
+        }
+        System.out.print("Enter duration: ");
+        int weekDuration;
+        try {
+            weekDuration = Integer.parseInt(scanner.nextLine());
+        } catch (RuntimeException e) {
+            System.out.println("Error: " + e.getMessage());
+            newCourse();
+            return;
+        }
+        Course course = courseDao.save(new Course(courseName, startDate, weekDuration));
+        System.out.println("Following course was added: " + course);
     }
 
     private void newStudent() {
-        System.out.println("Enter name: ");
+        System.out.print("Enter name: ");
         String name = scanner.nextLine();
         System.out.print("Enter email: ");
         String email = scanner.nextLine();
@@ -83,8 +109,37 @@ public class CommandLineInterface {
         System.out.println("Following student was added: " + student);
     }
 
+    //endregion
+    //region <Register>
+
     private void courseRegistration() {
+        System.out.print("Register or remove students from courses: \n" +
+                "1. Register student to a course \n" +
+                "2. Remove student from a course \n" +
+                "0. Main menu \n" +
+                "> ");
+
+        switch (scanner.nextLine()) {
+            case "1":
+                registerStudent();
+                break;
+            case "2":
+                removeStudent();
+                break;
+            case "0":
+                mainMenu();
+                return;
+        }
+        courseRegistration();
     }
+
+    private void registerStudent() {
+    }
+
+    private void removeStudent() {
+    }
+
+    //endregion
 
     private void read() {
     }
